fae4cccb51113e6e7c1aac7489fac17c50f383b5
iamhelenavd <helena_dorp@hotmail.com>
2024-12-10 21:22:25 +0100

    [feature]:(animation): add confetti and loader

---
+++ src/utils/animations/motion/threeDotsWave.tsx
@@ -0,0 +1,83 @@
+import { AnimationProps, motion } from "motion/react";
+
+const LoadingDot = {
+  display: "block",
+  width: "2rem",
+  height: "2rem",
+  backgroundColor: "white",
+  borderRadius: "50%",
+};
+
+const LoadingContainer = {
+  width: "10rem",
+  height: "5rem",
+  display: "flex",
+  justifyContent: "space-around",
+  gap: "1rem",
+};
+
+const ContainerVariants = {
+  initial: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+  animate: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const DotVariants = {
+  initial: {
+    y: "0%",
+  },
+  animate: {
+    y: "100%",
+  },
+};
+
+const DotTransition: AnimationProps['transition'] = {
+  duration: 0.5,
+  repeat: Infinity,
+  repeatType: "mirror",
+  ease: "easeInOut",
+};
+
+export default function ThreeDotsWave() {
+  return (
+    <div
+      style={{
+        paddingTop: "5rem",
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <motion.div
+        style={LoadingContainer}
+        variants={ContainerVariants}
+        initial="initial"
+        animate="animate"
+      >
+        <motion.span
+          style={LoadingDot}
+          variants={DotVariants}
+          transition={DotTransition}
+        />
+        <motion.span
+          style={LoadingDot}
+          variants={DotVariants}
+          transition={DotTransition}
+        />
+        <motion.span
+          style={LoadingDot}
+          variants={DotVariants}
+          transition={DotTransition}
+        />
+      </motion.div>
+    </div>
+  );
+}
